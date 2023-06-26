aa2ed79bf259acd01addd406103fa9b9f9e3ef0d
antony-lucide <antony.lucide@laplateforme.io>
2023-06-26 16:21:13 +0200

    Ceci est un prototype du jour01 de la runtrack Js.

---
+++ Jour01/Job04/script.js
@@ -0,0 +1,10 @@
+function Années(année) {
+    if (année / 4) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+console.log(Années(2023))
---
+++ Jour01/Job05/script.js
@@ -0,0 +1,9 @@
+function affichersjoursemaines() {
+    let tableau = "Lundi,Mardi,Mercredi,jeudi,vendredi,samedi,dimanche"
+
+    for (let i = 0;  i < tableau.length; i++) {
+        console.log(tableau)
+    }
+}
+
+affichersjoursemaines()
